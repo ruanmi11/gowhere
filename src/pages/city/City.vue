cd5a562e03bc435b19fe6f897585ed9c739281e8
xiaohua <565724897@qq.com>
2022-03-16 23:36:11 +0800

    city perfomance

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,30 +2,58 @@
     <div class="header">
         <city-header></city-header> 
         <city-search></city-search>
+        <city-list 
+           :cities="city" 
+           :hot="hotCities"
+           :letter="letter"
+        ></city-list>
+        <city-alpabate 
+           :cities="city"
+           @change="handleLetterChange"
+        ></city-alpabate>
     </div>
 </template>
 
 <script>
-// import axios from 'axios';
+import axios from 'axios';
 import CityHeader from './components/Header'; 
 import CitySearch from './components/Search';
-// import CityList from './components/List';
-// import CityAlpabate from './components/Alpabate';
+import CityList from './components/List';
+import CityAlpabate from './components/Alpabate';
 export default {
     name:'City',
     data(){
         return {
             city:{},
-            // hotCities:{},
-            // letter:''
+            hotCities:[],
+            letter:''
         }
     },
     components:{
         CityHeader,
         CitySearch,
-        // CityList,
-        // CityAlpabate
+        CityList,
+        CityAlpabate
     },
+    methods:{
+        getCityInfo(){
+            axios.get('/mock/city.json')
+            .then((res)=>{
+                res=res.data
+                if(res.ret && res.data){
+                    const data=res.data;
+                    this.city=data.cities
+                    this.hotCities=data.hotCities
+                }
+            })
+        },
+        handleLetterChange(letter){
+            this.letter=letter
+        }
+    },
+    mounted(){
+        this.getCityInfo()
+    }
 }
 </script>
 
